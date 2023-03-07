21636d07765478bc2584ffa6de6dab455dcaf160
David <david.assouline@Hotmail.com>
2023-03-07 17:19:16 -0500

    Added leaderboard to home

--- src/js/bootstrap.bundle.js
+++ src/js/bootstrap.bundle.js
@@ -3159,7 +3159,7 @@
     var scaleY = round(rect.height) / element.offsetHeight || 1;
     return scaleX !== 1 || scaleY !== 1;
   } // Returns the composite rect of an element relative to its offsetParent.
-  // Composite means it takes into account transforms as well as layout.
+  // Composite means it takes into account transforms as well as layout.handlebars.
 
 
   function getCompositeRect(elementOrVirtualElement, offsetParent, isFixed) {
